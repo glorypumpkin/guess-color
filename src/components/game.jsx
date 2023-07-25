6f59252b788a25bc984751630731220f2d5b11e2
glorypumpkin <sashadudar64@gmail.com>
2023-07-25 15:54:44 +0200

    Minor upgrades

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,4 +1,5 @@
 import style from './game.module.css';
+import { useState } from 'react';
 
 export default function Game({ gameConfig }) {
     console.log(gameConfig);
@@ -10,20 +11,51 @@ export default function Game({ gameConfig }) {
     }
 }
 
+function GenerateRandomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function GuessWindow() {
+    return (
+        <div className={style.guess}>
+            <div className={style.text_inboxes}>
+                <input className={`${style.text_input} ${style.red_input}`} type="text" />
+                <input className={`${style.text_input} ${style.green_input}`} type="text" />
+                <input className={`${style.text_input} ${style.blue_input}`} type="text" />
+            </div>
+            <div className={style.game_buttons}>
+                <button className={style.game_button}>Reset</button>
+                <button className={style.game_button}>Submit</button>
+            </div>
+        </div>
+    )
+}
+
 function RGBMode1() {
+
+    const [colorDisplay, setColorDisplay] = useState(GenerateRandomColor());
+
+    console.log(colorDisplay);
+
+    function onNewGameClicked() {
+
+        setColorDisplay(GenerateRandomColor());
+    }
+
     return (
         <div className={style.container}>
             <h1>RGB Mode 1</h1>
+            <button className={style.new_game_button} onClick={onNewGameClicked}>New Game</button>
             <div className={style.game_container}>
                 <div className={style.color_container}>
-                    <div className={style.square}></div>
+                    <div className={style.square} style={{ backgroundColor: colorDisplay }}></div>
                 </div>
                 <div className={style.guesses_container}>
-                    <div className={style.guess}></div>
-                    <div className={style.guess}></div>
-                    <div className={style.guess}></div>
-                    <div className={style.guess}></div>
-                    <div className={style.guess}></div>
+                    <GuessWindow></GuessWindow>
+
                 </div>
             </div>
         </div>
