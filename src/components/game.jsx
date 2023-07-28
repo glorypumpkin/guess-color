3e23b3b800aeed3513c592a7360bde09ac66b2c0
glorypumpkin <sashadudar64@gmail.com>
2023-07-28 18:13:01 +0200

    Minor update

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,6 +1,9 @@
 import style from './game.module.css';
 import { useState } from 'react';
 
+
+const guessLimit = 5;
+
 export default function Game({ gameConfig }) {
     console.log(gameConfig);
     if (gameConfig.color === 'rgb' && gameConfig.mode === 'mode 1') {
@@ -17,19 +20,70 @@ function GenerateRandomColor() {
     let b = Math.floor(Math.random() * 256);
     return `rgb(${r}, ${g}, ${b})`;
 }
+// guessState: active, hidden, late
+function GuessWindow({ guessState, onGuessSubmitted }) {
 
-function GuessWindow() {
-    return (
-        <div className={style.guess}>
+    const [r, setR] = useState(0);
+    const [g, setG] = useState(0);
+    const [b, setB] = useState(0);
+
+
+
+    function onSubmit() {
+        // get input values from text boxes
+        const inputValues = {
+            r: r,
+            g: g,
+            b: b
+        }
+        // call onGuessSubmitted with input values
+        onGuessSubmitted(inputValues);
+    }
+
+    const activeGuess = (
+        <>
+            <div className={style.text_inboxes}>
+                <input className={`${style.text_input} ${style.red_input}`} type="text" onChange={
+                    (e) => {
+                        setR(e.target.value);
+                    }
+                } />
+                <input className={`${style.text_input} ${style.green_input}`} type="text" onChange={
+                    (e) => {
+                        setG(e.target.value);
+                    }
+                } />
+                <input className={`${style.text_input} ${style.blue_input}`} type="text" onChange={
+                    (e) => {
+                        setB(e.target.value);
+                    }
+                } />
+            </div>
+            <div className={style.game_buttons}>
+                <button className={style.game_button}>Reset</button>
+                <button className={style.game_button} onClick={onSubmit}>Submit</button>
+            </div>
+        </>
+    )
+
+    const hiddenGuess = null;
+
+    const lateGuess = (
+        <>
             <div className={style.text_inboxes}>
                 <input className={`${style.text_input} ${style.red_input}`} type="text" />
                 <input className={`${style.text_input} ${style.green_input}`} type="text" />
                 <input className={`${style.text_input} ${style.blue_input}`} type="text" />
             </div>
-            <div className={style.game_buttons}>
-                <button className={style.game_button}>Reset</button>
-                <button className={style.game_button}>Submit</button>
-            </div>
+            <div className={style.game_buttons}> </div>
+        </>
+    )
+
+    return (
+        <div className={style.guess}>
+            {guessState === 'active' && activeGuess}
+            {guessState === 'hidden' && hiddenGuess}
+            {guessState === 'late' && lateGuess}
         </div>
     )
 }
@@ -37,12 +91,32 @@ function GuessWindow() {
 function RGBMode1() {
 
     const [colorDisplay, setColorDisplay] = useState(GenerateRandomColor());
-
-    console.log(colorDisplay);
+    const [activeGuessIndex, setActiveGuessIndex] = useState(0);
 
     function onNewGameClicked() {
-
+        setActiveGuessIndex(0);
         setColorDisplay(GenerateRandomColor());
+    }
+
+    function showGuesses() {
+        let guesses = [];
+        for (let i = 0; i < guessLimit; i++) {
+            if (i === activeGuessIndex) {
+                guesses.push(<GuessWindow onGuessSubmitted={onGuessSubmitted} key={i} guessState="active"></GuessWindow>)
+            }
+            else if (i > activeGuessIndex) {
+                guesses.push(<GuessWindow key={i} guessState="hidden"></GuessWindow>)
+            }
+            else {
+                guesses.push(<GuessWindow key={i} guessState="late"></GuessWindow>)
+            }
+        }
+        return guesses;
+    }
+    //inputValues = {r, g, b} int
+    function onGuessSubmitted(inputValues) {
+        console.log(inputValues);
+        setActiveGuessIndex(activeGuessIndex + 1);
     }
 
     return (
@@ -54,8 +128,7 @@ function RGBMode1() {
                     <div className={style.square} style={{ backgroundColor: colorDisplay }}></div>
                 </div>
                 <div className={style.guesses_container}>
-                    <GuessWindow></GuessWindow>
-
+                    {showGuesses()}
                 </div>
             </div>
         </div>
